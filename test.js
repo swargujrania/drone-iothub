2eac7df0ed11d45c56e7d5e72aecfcb5f6bb619f
Nitish Victor <nithishvictor@gmail.com>
2015-11-18 10:15:17 +0000

    @Omg@

--- test.js
+++ test.js
@@ -4,40 +4,41 @@ var droneToCloud = require('./DroneToCloud');
 
 
 function parseCommand(move){
-	if(move.type == 'left'){
+	if(move.Direction == 'left'){
 		droneClient.left(move.thrust);
     	}
-    	if(move.type == 'right'){
+    	if(move.Direction == 'right'){
 		droneClient.right(move.thrust);
     	}
-    	if(move.type == 'up'){
+    	if(move.Direction == 'up'){
 		droneClient.up(move.up);
     	}
-    	if(move.type == 'down'){
+    	if(move.Direction == 'down'){
 		droneClient.down(move.thrust);
     	}
-    	if(move.type == 'front'){
+    	if(move.Direction == 'front'){
 		droneClient.front(move.thrust);
     	}
-    	if(move.type == 'back'){
+    	if(move.Direction== 'back'){
 		droneClient.back(move.thrust);
     	}
-   	if(move.type == 'left'){
+   	if(move.Direction == 'left'){
 		droneClient.left(move.thrust);
     	}
-   	if(move.type == 'clockwise'){
+   	if(move.Direction == 'clock'){
 		droneClient.clockwise(move.thrust);
     	}
-    	if(move.type == 'counterclockwise'){
+    	if(move.Direction == 'counterclock'){
 		droneClient.counterClockwise(move.thrust);
    	}
 	droneClient
-	.after(move.time, function(){
+	.after(move.TimeInMilliSecond, function(){
 		this.stop();
 	});
 }
 
 function parseOtherCommand(type){
+	console.log(type);
 	if(type=='takeoff'){
 		droneClient.takeoff();
 	}
@@ -62,16 +63,15 @@ droneClient.after(5000, function(){
 
 var onReceive = function(data){
 	console.log(data);
-	
-	//if(data.type=="movement"){
-	//	parseCommand(data);
-	//}
-	//else if(data.type=="droneData"){
-		//
-	//}
-	//else{
-	//	parseOtherCommand(type.type);
-	//}
+	if(data.Type=="move"){
+		parseCommand(data);
+	}
+	else if(data.Type=="droneData"){
+		droneToCloud.sendData();
+	}
+	else{
+		parseOtherCommand(data.Type);
+	}
 }
 
 droneToCloud.startReceiving(onReceive);
